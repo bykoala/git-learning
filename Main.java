98dadc2ad2ebb3fbda080888db2493dbbc63ea3e
bykoala <by.su@qq.com>
2018-08-03 22:41:41 +0800

    首次提交

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main{
+	public static void main(String[] args){
+		System.out.println("hello ,git");
+	}
+
+	public static int sum(int num1,int num2){
+		return num1 + num2;
+	
+	}
+	public static int plus(int num1,int num2){
+		return num1 + num2;
+	}
+}
